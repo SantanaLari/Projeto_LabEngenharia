5a755f076a579f6dc84f1402b0d14f1f092585cf
SantanaLari <88253169+SantanaLari@users.noreply.github.com>
2022-11-28 17:07:57 -0300

    Add files via upload

---
+++ Livraria/src/model/Login.java
@@ -0,0 +1,28 @@
+package model;
+
+public class Login {
+		
+	private String email;
+	private String senha;
+	private String opcao; 
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public String getOpcao() {
+		return opcao;
+	}
+	public void setOpcao(String opcao) {
+		this.opcao = opcao;
+	}
+	
+}
